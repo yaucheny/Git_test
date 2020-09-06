3057d77571b3d57bd0dcf351d586a8dd74d4c8a2
Yauheni Markevich <2879105@gmail.com>
2020-09-06 17:47:56 +0300

    commit_1

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main
+{
+    //psvm+ Tab, sout+tab
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/com/company/thema4.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class thema4 {
+    public static void main(String[] args) {
+
+        {
+            double a = (Math.random()) * 156 + 5;
+            int b = (int) a;
+            if (b >25), (b < 100) {
+            System.out.println("Результат равен " + b);
+            System.out.println("и принадлежит отрезку (25;100) " + b);
+            else
+        }
+    }}
