59094e2271a94b12fcc05732c7f17e1e18a74e6e
Yauheni Markevich <2879105@gmail.com>
2020-09-08 20:58:25 +0300

    commit_1

---
+++ src/com/company/thema5.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class thema5 {
+    public static void main(String[] args) {
+        for (int i = 1000; i < 10000; i = i + 3)
+        {
+            System.out.println(" ," + i);
+        }
+    }
+}
