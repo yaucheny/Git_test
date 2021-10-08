bb3f81b6044d5cb78f73acbe9eedd5b90e2dc714
yaucheny <2879105@gmail.com>
2021-10-08 14:57:46 +0300

    feature-a branch

--- src/com/company/Kursy_YauheniEkuntsevich/Lesson7_Collections/Zadaniya/NotMap.java
+++ src/com/company/Kursy_YauheniEkuntsevich/Lesson7_Collections/Zadaniya/NotMap.java
@@ -4,4 +4,10 @@ public class NotMap {
     public static void main(String[] args) {
         System.out.println("GITHUB");
     }
+    public void forCommit(){
+        int a=1;
+        int b=1;
+        int c=1;
+        int d=1;
+    }
 }
